f2eab2076959215c89457734c633098aae9e3d39
MehdiEidi <mehdiadq@gmail.com>
2021-09-08 23:11:51 +0430

    directory walk added

--- main.go
+++ main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
-	ffprobe "github.com/vansante/go-ffprobe"
+	"io/fs"
 	"log"
+	"path/filepath"
+	"strings"
 	"time"
+
+	ffprobe "github.com/vansante/go-ffprobe"
 )
 
 func main() {
-	data, err := ffprobe.GetProbeData("./b.mkv", 120000*time.Millisecond)
+	total := 0.0
+
+	err := filepath.Walk("./dir", func(p string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			ext := strings.Split(info.Name(), ".")[1]
+
+			if ext == "mkv" || ext == "mp4" {
+				fmt.Printf("%s	|	%.3f minutes\n", p, getDuration(p))
+				total += getDuration(p)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Total duration of all videos: %.2f minutes", total)
+}
+
+func getDuration(p string) float64 {
+	data, err := ffprobe.GetProbeData(p, 120000*time.Millisecond)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	duration := data.Format.Duration().Seconds() / 60
 
-	fmt.Println(duration)
+	return duration
 }
