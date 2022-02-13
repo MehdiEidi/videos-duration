cb40008acaac293bf9ae4f260f301c9cddcba0ea
MehdiEidi <mehdiadq@gmail.com>
2022-02-13 13:43:55 +0330

    refactored

--- main.go
+++ main.go
@@ -6,41 +6,10 @@ import (
 	"io/fs"
 	"log"
 	"path/filepath"
-	"strings"
 	"time"
 
 	ffprobe "github.com/vansante/go-ffprobe"
 )
-
-func main() {
-	dirPath := flag.String("path", ".", "Path of the directory to check videos in it.")
-	flag.Parse()
-
-	total := 0.0
-
-	err := filepath.Walk(*dirPath, func(p string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !info.IsDir() {
-			if ext := strings.Split(info.Name(), "."); len(ext) > 1 {
-				if ext[len(ext)-1] == "mkv" || ext[len(ext)-1] == "mp4" || ext[len(ext)-1] == "wmv" || ext[len(ext)-1] == "avi" {
-					fmt.Printf("%s	|	%.3f minutes\n", p, getDuration(p))
-					total += getDuration(p)
-				}
-			}
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Total duration of all videos: %.2f minutes", total)
-}
 
 func getDuration(p string) float64 {
 	data, err := ffprobe.GetProbeData(p, 120000*time.Millisecond)
@@ -51,4 +20,36 @@ func getDuration(p string) float64 {
 	duration := data.Format.Duration().Seconds() / 60
 
 	return duration
+}
+
+func main() {
+	dirPath := flag.String("path", ".", "Path of the directory to check videos in it.")
+	flag.Parse()
+
+	var total float64
+
+	err := filepath.Walk(*dirPath, func(p string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			ext := filepath.Ext(p)
+
+			if ext == ".mkv" || ext == ".mp4" || ext == ".wmv" || ext == ".avi" || ext == ".ts" {
+				fmt.Printf("%s --> %.3f minutes\n", p, getDuration(p))
+				total += getDuration(p)
+			}
+
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("======================================================================\n")
+	fmt.Printf("Total duration of all videos: %.2f minutes\n", total)
 }
